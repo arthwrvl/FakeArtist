3c6e4883ee17fe5479026dfca78a686cd90c631b
Arthur Valério <31370475+arthwrvl@users.noreply.github.com>
2023-03-25 01:24:32 -0300

    add random drawing on menu card

--- lib/widgets/menu_name.dart
+++ lib/widgets/menu_name.dart
@@ -1,6 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fake_artist/styles/drawings.dart';
 
 class MenuName extends StatefulWidget {
   const MenuName({super.key});
@@ -38,12 +40,14 @@ class PolaroidMenu extends StatefulWidget {
 }
 
 class _PolaroidMenuState extends State<PolaroidMenu> {
+  int rand = Random().nextInt(8);
+  var drawings = [line1, line2, line3, line4, line5, line6, line7];
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-      width: size.width * 0.6,
-      height: size.width * 0.6 * 1.4,
+      width: size.width * 0.56,
+      height: size.width * 0.56 * 1.4,
       decoration: BoxDecoration(
           border: Border.all(color: Colors.black, width: 6),
           color: const Color(0xFFE8F1D7),
@@ -51,21 +55,22 @@ class _PolaroidMenuState extends State<PolaroidMenu> {
             BoxShadow(color: Colors.black, offset: Offset(5, 5))
           ]),
       child: Column(children: [
-        SizedBox(height: size.width * 0.6 * .04),
+        SizedBox(height: size.width * 0.56 * .04),
         Stack(
+          alignment: Alignment.center,
           children: [
             Container(
-              width: size.width * 0.6 * 0.87,
-              height: size.width * 0.6 * 0.87,
+              width: size.width * 0.56 * 0.87,
+              height: size.width * 0.56 * 0.87,
               color: const Color(0xFF95F0C5),
             ),
-            //Image
+            SvgPicture.asset(drawings[rand], width: size.width * 0.56 * 0.87),
           ],
         ),
         Align(
           alignment: Alignment.centerLeft,
           child: Padding(
-            padding: EdgeInsets.only(left: size.width * 0.6 * 0.04),
+            padding: EdgeInsets.only(left: size.width * 0.56 * 0.04),
             child: const Text(
               "FAKE\nARTIST",
               style: TextStyle(
